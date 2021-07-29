35fff02db1047809cbb6cfa3241fc7cfa9c258bd
jolim <jolim@student.42seoul.fr>
2021-07-29 14:18:59 +0900

    ConfigParser changed for corresponding cpp

--- ConfigParser.hpp
+++ ConfigParser.hpp
@@ -19,7 +19,7 @@ class ConfigParser {
 		static const std::string			location_config_arr[7];
 		static const std::set<std::string>	server_config;
 		static const std::set<std::string>	location_config;
-		static const char*					status_code_set[];
+		static const char*					status_code_arr[];
 		static std::map<std::string, const char*>	status_code_map;
 		static std::ifstream				config_file;
 
