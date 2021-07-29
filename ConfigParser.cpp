9f3b35361b1acab377e605092a7f70f6fbb9c42f
jolim <jolim@student.42seoul.fr>
2021-07-29 14:11:43 +0900

    ConfigParser::locationBlock implemented

--- ConfigParser.cpp
+++ ConfigParser.cpp
@@ -31,12 +31,12 @@ const std::set<std::string>	ConfigParser::location_config(
 		+ sizeof(ConfigParser::location_config_arr) / sizeof(std::string)
 );
 
-const char*	ConfigParser::status_code_arr[] = {C100, C101, C200, C201, C202,
+stat_type	ConfigParser::status_code_arr[] = {C100, C101, C200, C201, C202,
 	C203, C204, C205, C206, C300, C301, C302, C303, C304, C305, C307, C400,
 	C401, C402, C403, C404, C405, C406, C407, C408, C409, C410, C411, C412,
 	C413, C414, C415, C416, C417, C500, C501, C502, C503, C504, C505};
 
-std::map<std::string, const char*>	ConfigParser::status_code_map;
+std::map<std::string, stat_type>	ConfigParser::status_code_map;
 
 void	ConfigParser::initStatusCodeMap() {
 
@@ -169,12 +169,11 @@ int	ConfigParser::httpBlock(std::vector<Server>& servers) {
 	if (getIntoBlock("http"));
 		return ERROR;
 
-	Server*		server;
 	std::string	trails = "";
 	int			ret = 0;
 
 	while (!ret) {
-		ret = ConfigParser::serverBlock(server);
+		ret = ConfigParser::serverBlock(servers);
 		if (ret == ERROR)
 			return ERROR;
 		// what does it do when there is no server block?
@@ -194,11 +193,14 @@ int	ConfigParser::locationBlock(std::vector<Location>& locations, std::string& l
 	std::string							path = line;
 	std::string							root = ""; // when there is no root
 	std::vector<std::string>			indexes(0); // when there is no index pages
-	bool								auto_index = false; // when auto_index is not present, the default value is false.
-	std::map<std::string, std::string>	error_pages;
+	bool								auto_index = false; // the default value is false.
+	bool								auto_index_check = false; // to check if there is more than one auto_index.
+	std::map<stat_type, std::string>	error_pages;
 	Method								methods_allowed = GET; // default method is get
+	bool								method_allowed_check = false; // to check duplicated directives
 	std::map<std::string, std::string>	cgi_infos;
-	std::pair<const char*, std::string>	return_to;
+	std::pair<stat_type, std::string>	return_to;
+	bool								return_check = false; // to check duplicated return
 
 	// Elements included in one line.
 	std::vector<std::string>	elements;
@@ -212,52 +214,130 @@ int	ConfigParser::locationBlock(std::vector<Location>& locations, std::string& l
 
 		if (!elements[0].compare("root")) {
 			if (elements.size() != 2) {
-				std::cerr << "ConfigParser: " << "locationBlock: " << SEMANTIC_ERR << std::endl;
+				std::cerr << "ConfigParser: " << "locationBlock: " << "root: "<< SEMANTIC_ERR << std::endl;
 				return ERROR;
 			}
 			if (root.length()) {
-				std::cerr << "ConfigParser: " << "locationBlock: " << NAME_DUP_ERR << std::endl;
+				std::cerr << "ConfigParser: " << "locationBlock: " << "root: " << NAME_DUP_ERR << std::endl;
 				return ERROR;
 			}
 			root = elements[1];
+
 		} else if (!elements[0].compare("error_page")) {
 			if (!(elements.size() % 2)) {
-				std::cerr << "ConfigParser: " << "locationBlock: " << SEMANTIC_ERR << std::endl;
+				std::cerr << "ConfigParser: " << "locationBlock: " << "error_page:" << SEMANTIC_ERR << std::endl;
 			}
+			// error_page 1 2 3 4 a b c d :config
+			// 0          1 2 3 4 5 6 7 8 :index
 			for (size_t i = 1; i <= elements.size() / 2; ++i) {
-				std::map<std::string, const char*>::iterator error_code
+				std::map<std::string, stat_type>::iterator error_code
 					= ConfigParser::status_code_map.find(elements[i]);
-// error_page 1 2 3 4 a b c d
-// 0          1 2 3 4 5 6 7 8
+				if (error_code == ConfigParser::status_code_map.end()) {
+					std::cerr << "ConfigParser: " << "locationBlock: " << "error_page: " << SEMANTIC_ERR << std::endl;
+					return ERROR;
+				}
+				// if the code already exists
+				if (error_pages.count((*error_code).second)) {
+					std::cerr << "ConfigParser: " << "locationBlock: " << "error_page: " << NAME_DUP_ERR << std::endl;
+					return ERROR;
+				}
+				error_pages[(*error_code).second] = elements[i * 2];
 			}
+
 		} else if (!elements[0].compare("index")) {
+			if (elements.size() == 1) {
+				std::cerr << "ConfigParer: " << "locationBlock: " << "index: " << NO_ENTITY_ERR << std::endl;
+			}
+			for (size_t i = 1; i < elements.size(); ++i)
+				indexes.push_back(elements[i]);
+
 		} else if (!elements[0].compare("auto_index")) {
+			if (auto_index_check) {
+				std::cerr << "ConfigParser: " << "locationBlock: " << "auto_index: " << NAME_DUP_ERR << std::endl;
+			}
 			if (elements.size() != 2) {
-				std::cerr << "ConfigParser: " << "locationBlock: " << SEMANTIC_ERR << std::endl;
+				std::cerr << "ConfigParser: " << "locationBlock: " << "auto_index: " << SEMANTIC_ERR << std::endl;
 				return ERROR;
 			}
+			if (!elements[1].compare("on")) {
+				auto_index = true;
+			} else if (!elements[1].compare("off")) {
+				auto_index = false;
+			} else {
+				std::cerr << "ConfigParser: " << "locationBlock: " << "auto_index: " << SEMANTIC_ERR << std::endl;
+				return ERROR;
+			}
+			auto_index_check = true;
+
 		} else if (!elements[0].compare("method_allowed")) {
-			if (elements.size() > 4) {
-				std::cerr << "ConfigParser: " << "locationBlock: " << SEMANTIC_ERR << std::endl;
+			if (method_allowed_check) {
+				std::cerr << "ConfigParser: " << "locationBlock: " << "method_allowed: " << NAME_DUP_ERR << std::endl;
 				return ERROR;
 			}
+			if (elements.size() > 4) {
+				std::cerr << "ConfigParser: " << "locationBlock: " << "method_allowed: " << SEMANTIC_ERR << std::endl;
+				return ERROR;
+			}
+			bool	get = false, post = false, del = false;
+			for (size_t i = 1; i < elements.size(); ++i) {
+				if (get || post || del) {
+					std::cerr << "ConfigParser: " << "locationBlock: "
+						<< "method_allowed: " << NAME_DUP_ERR << std::endl;
+					return ERROR;
+				}
+				if (!elements[i].compare("GET")) {
+					methods_allowed |= GET;
+					get = true;
+				} else if (!elements[i].compare("POST")) {
+					methods_allowed |= POST;
+					post = true;
+				} else if (!elements[i].compare("DELETE")) {
+					methods_allowed |= DELETE;
+					del = true;
+				} else {
+					std::cerr << "ConfigParser: " << "locationBlock: "
+						<< "method_allowed: " << SEMANTIC_ERR << std::endl;
+				}
+			}
+			method_allowed_check = true;
+
 		} else if (!elements[0].compare("cgi_info")) {
 			if (elements.size() != 3) {
-				std::cerr << "ConfigParser: " << "locationBlock: " << SEMANTIC_ERR << std::endl;
+				std::cerr << "ConfigParser: " << "locationBlock: " << "cgi_info: " << SEMANTIC_ERR << std::endl;
 				return ERROR;
 			}
+			if (cgi_infos.count(elements[1])) {
+				std::cerr << "ConfigParser: " << "locationBlock: " << "cgi_info: " << NAME_DUP_ERR << std::endl;
+				return ERROR;
+			}
+			cgi_infos[elements[1]] = elements[2];
+
 		} else { // "return"
+			if (return_check) {
+				std::cerr << "ConfigParser: " << "locationBlock: " << "return: " << NAME_DUP_ERR<< std::endl;
+			}
 			if (elements.size() != 3) {
-				std::cerr << "ConfigParser: " << "locationBlock: " << SEMANTIC_ERR << std::endl;
+				std::cerr << "ConfigParser: " << "locationBlock: " << "return: " << SEMANTIC_ERR << std::endl;
 				return ERROR;
 			}
+			if (!status_code_map.count(elements[1])) {
+				std::cerr << "ConfigParser: " << "locationBlock: " << "return: " << SEMANTIC_ERR << std::endl;
+				return ERROR;
+			}
+			return_to.first = status_code_map[elements[1]];
+			return_to.second = elements[2];
+			return_check = true;
 		}
 	}
 	if (elements[0].compare("}")) {
 		std::cerr << "ConfigParser: " << "locationBlock: " << SEMANTIC_ERR << std::endl;
 		return ERROR;
 	}
-
+	if (!root.length()) {
+		std::cerr << "ConfigParser: " << "locationBlock: " << "root: " << NO_ENTITY_ERR << std::endl;
+		return ERROR;
+	}
+	locations.push_back(Location(path, root, indexes, auto_index, error_pages, methods_allowed, cgi_infos, return_to));
 }
 
 int ConfigParser::readFile(std::vector<Server>& servers, const char* config_path) {
