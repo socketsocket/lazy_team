bd0838bd0586eb632276ab69aa46519a440d05ea
evelon <ezemango@gmail.com>
2021-08-08 16:31:42 +0900

    GciConnector.hpp initialized

---
+++ CgiConnector.hpp
@@ -0,0 +1,13 @@
+#ifndef WEBSERV_CGICONNECTOR_HPP
+#define WEBSERV_CGICONNECTOR_HPP
+
+class CgiConnector {
+	private:
+	public:
+		CgiConnector();
+		CgiConnector(const CgiConnector& ref);
+		~CgiConnector();
+		CgiConnector&	operator=(const CgiConnector& ref);
+};
+
+#endif
